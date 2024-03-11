0406e0b7a06bd9c61df0eb8968457e959091688c
robiul99siyam <siyamrobiulislam@gmail.com>
2024-03-11 12:02:10 +0600

    order model update now

--- order/admin.py
+++ order/admin.py
@@ -8,12 +8,12 @@ from django.template.loader import render_to_string
 
 
 class ProductOreder(admin.ModelAdmin):
-    list_display = ['product_name','current_address','phone_number', 'product_dealybary']
+    list_display = ['product_name','current_address','phone_number', 'product_dealybary','full_name','email']
     def save_model(self,request,obj,form,change):
         if obj.product_dealybary == 'Running':
             email_subject = "Tree Shop Order Now "
-            email_body = render_to_string('oreder.html',{'user':obj.user})
-            email = EmailMultiAlternatives(email_subject,'',to=[obj.user.email])
+            email_body = render_to_string('oreder.html',{'user':obj.full_name})
+            email = EmailMultiAlternatives(email_subject,'',to=[obj.email])
             email.attach_alternative(email_body,'text/html')
             email.send()
 
--- order/models.py
+++ order/models.py
@@ -12,9 +12,14 @@ class OrderProduct(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     product_dealybary = models.CharField(max_length=40,choices=PRODUCT_DEALYBARY,default='Pending')
     product_name=models.CharField(max_length=40)
-    current_address = models.CharField(max_length=40)
+    current_address = models.CharField(max_length=100 , null=True)
     phone_number = models.CharField(max_length=40)
     cencal = models.BooleanField(default=False)
+    full_name = models.CharField(max_length=100,null=True)
+    email = models.EmailField(max_length=100,null=True)
+    city = models.CharField(max_length=100,null=True)
+    zip_code = models.CharField(max_length=100,null=True)
+    credit_number = models.CharField(max_length=100,null=True)
 
 
 
