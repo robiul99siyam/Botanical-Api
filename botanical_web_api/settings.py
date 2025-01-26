975b98b1c80ecb01b04791986c8f809b276a7325
robiul99siyam <siyamrobiulislam@gmail.com>
2025-01-26 20:18:45 +0600

    add the email app  password

--- botanical_web_api/settings.py
+++ botanical_web_api/settings.py
@@ -154,16 +154,6 @@ EMAIL_HOST_USER = env("EMAIL")
 EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT"),
-    }
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
